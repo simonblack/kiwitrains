74cabd2f91c7e6c3646da6ab167bdf19d09c2c40
Simon Black <simon@cerneo.org>
2018-03-23 22:00:47 -0400

    Implemention of - Breadth-First-Search, and - Dijkstra's shortest path

--- kiwitrains/lib.py
+++ kiwitrains/lib.py
@@ -1,13 +1,3 @@
-#!/usr/bin/env python3
-"""
-    Kiwiland Railroad Transit library.
-
-    This module provides a TransitMap object for the querying and 
-    manipulation of train route data for use at Kiwiland Railroads
-
-"""
-
-
 class TransitMap(dict):
     """
     An adjacency map-type object for interacting with train routes.
@@ -19,10 +9,17 @@ class TransitMap(dict):
     """
     
     def __init__(self, csv):
-        # convert csv to adjacency mapping
+        # convert csv to adjacency mapping on self
+        for route in csv.split(', '):
+            
+            if route[0] not in self:
+                self[route[0]] = {}
+            
+            self[route[0]][route[1]] = int(route[2])
     
     def __bfs(self, start, end, min_depth=1, max_depth=5):
-        """breadth-first search.
+        """
+        Perform a breadth-first search.
         
         Args:
             start (str): The root node.
@@ -32,8 +29,36 @@ class TransitMap(dict):
             min_depth (int): The minimum traversal depth. Defaults to 1.
             max_depth (int): The maximum traversal depth. Defaults to 5.
         
+        Returns: 
+            paths (list): A list of discovered paths.
         
         """
+        paths = []
+        visited = []
+        queue = [start]
+        
+        while queue:
+            path = queue.pop(0)
+            node = path[-1]
+            
+            if node == end and len(queue):
+                if len(path) <= max_depth:
+                    if len(path) >= min_depth:
+                        paths.append(path)
+                else:
+                    break # stop traversal if we reach length limits
+            
+            if node in visited:
+                if len(path) > max_depth:
+                    continue
+            
+            for neighbour in self[node].keys():
+                _next = list(path)
+                _next.append(neighbour)
+                queue.append(_next)
+                visited.append(node)
+        
+        return paths
     
     def __dijkstra(self, start):
         """
@@ -42,7 +67,43 @@ class TransitMap(dict):
         Args:
             start (str): The root node.
         
+        Returns:
+            (tuple): A tuple containing:
+
+                visited (dict): Distances from root node to other nodes.
+                chain (dict): A dict representation of the traversed path.
+            
         """
+        visited = {start:0}
+        queue = set(self.keys())
+        chain = {}
+        
+        while queue: 
+            root = None
+            
+            for node in queue:
+                if node in visited and not root:
+                    root = node
+            
+            if root is None:
+                break
+            
+            queue.remove(root)
+            current_weight = visited[root]
+            
+            # Remove the root node from the visited nodes map in order to
+            # find paths that start and end with the same node.
+            if not visited[root]:
+                del visited[root]
+            
+            for edge in self[root]:
+                weight = self[root][edge] + current_weight
+                
+                if (edge not in visited or visited[edge] > weight):
+                    visited[edge] = weight
+                    chain[edge] = root
+            
+        return visited, chain
     
     def count_trips_by_distance(self, start, end, min_length=0, max_length=20):
         """
